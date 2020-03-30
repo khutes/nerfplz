bcfb50fece4777fe6915bd56f6892d2a9e54ba41
Nick Nelson <nnelson1601@tamu.edu>
2020-03-30 10:31:39 -0500

    Debugging message passing

--- rpi.py
+++ rpi.py
@@ -8,13 +8,24 @@ threads = []
 
 light = LED(12)
 
+def blinkLED(numTimes):
+    for i in range(numTimes):
+        light.on()
+        sleep(.25)
+        light.off()
+        sleep(.25)
+    return
+
 # Creating the camera server is currently a blocking function until a connection is established
 camSock = network.createCameraServer()
+blinkLED(1)
 msgSock = network.createMessageServer()
+blinkLED(2)
 
 t = threading.Thread(target=camServ.startCameraFeed, args=(camSock,))
 threads.append(t)
 t.start()
+blinkLED(3)
 
 while True:
     try:
