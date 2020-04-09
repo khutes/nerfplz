d76977d24a74331c131bf72f7aa115e9fc4d44a6
Nick Nelson <nnelson1601@gmail.com>
2020-04-09 00:00:54 -0500

    Debug from pi

--- config/message_parser.py
+++ config/message_parser.py
@@ -8,42 +8,45 @@ perform the task indicated.
 
 def parse(sock, car):
 
-	# Move this block into a new module/thread before starting camera feed
-	while True:
-		try:
-			msg = sock.receive()
+    # Move this block into a new module/thread before starting camera feed
+    while True:
+        try:
+            msg = sock.receive()
 
-			if msg == "fire":
-				car.fire()
-			elif msg == "quit":
-				print("quit")
-			else:
-				value = int(msg[2:])
-				if "fd" in msg:
-					if value == 0:
-						car.stop()
-					else:
-						car.driveForward(value)
-				elif "tl" in msg:
-					car.steerLeft(value)
-				elif "bd" in msg:
-					car.driveBackward(value)
-				elif "tr" in msg:
-					car.steerRight(value)
-				elif "cu" in msg:
-					car.lookUp(value)
-				elif "cl" in msg:
-					car.lookLeft(value)
-				elif "cd" in msg:
-					car.lookDown(value)
-				elif "cr" in msg:
-					car.lookRight(value)
-				else:
-					print("wrong input")
-		except:
-			cfg.ALIVE = False
-			print("Error. Closing connection...")
-			del sock
-			break
+            if msg == "fire":
+                car.fire()
+            elif msg == "quit":
+                print("quit")
+            else:
+                value = int(msg[2:])
+                if "fd" in msg:
+                    if value == 0:
+                        car.stop()
+                    else:
+                        car.driveForward(value)
+                elif "tl" in msg:
+                    car.steerLeft(value)
+                elif "bd" in msg:
+                    if value == 0:
+                        car.stop()
+                    else:
+                        car.driveBackward(value)
+                elif "tr" in msg:
+                    car.steerRight(value)
+                elif "cu" in msg:
+                    car.lookUp(value)
+                elif "cl" in msg:
+                    car.lookLeft(value)
+                elif "cd" in msg:
+                    car.lookDown(value)
+                elif "cr" in msg:
+                    car.lookRight(value)
+                else:
+                    print("wrong input")
+        except:
+            cfg.ALIVE = False
+            print("Error. Closing connection...")
+            del sock
+            break
 
 
