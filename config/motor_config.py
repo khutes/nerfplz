fc0ad6ce5d557c75bf9c7cc3e0ab3ecc36e88a9a
Nick Nelson <nnelson1601@tamu.edu>
2020-04-24 14:53:47 -0500

    in2 5

--- config/motor_config.py
+++ config/motor_config.py
@@ -15,7 +15,7 @@ def init(car):
     # name, in1, in2, ena, init speed, min speed, max speed
     car.addDC("BackMotor", 16, 20, 21, 40, 15, 100)
     car.addDC("TurretMotor", 19, 13, 26, 15)
-    car.addDC("FireMotor", 6, 11, 14, 15, 15, 50)
+    car.addDC("FireMotor", 6, 5, 14, 15, 15, 75)
 
     # name, GPIO, init angle, min angle, max angle, increment
     # car.addServo("FrontServo", 17, increment=5)
