eb25ce2d665b9616ea7e881b04dbbfe0b9c62fec
Nick Nelson <nnelson1601@tamu.edu>
2020-04-24 00:43:32 -0500

    Working on DCs now

--- config/motor_config.py
+++ config/motor_config.py
@@ -11,7 +11,7 @@ SERVO_MAX_ANGLE = 135
 SERVO_ANGLE_INC = 1
 
 def init(car):
-    # car.addDC("BackMotor", 24, 23, 25, 50)
+    car.addDC("BackMotor", 23, 24, 25, 50)
     car.addServo("FrontServo", 17, 90)
     # Add motors here as desired
     car.printMotors()
