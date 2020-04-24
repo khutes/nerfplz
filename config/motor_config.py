2eb2a9e827210b725c17b3e03956f66d55df9ec7
Nick Nelson <nnelson1601@tamu.edu>
2020-04-24 00:46:14 -0500

    speed adj

--- config/motor_config.py
+++ config/motor_config.py
@@ -2,16 +2,16 @@
 
 DEFAULT_HERTZ = 50
 
-DC_MAX_SPEED = 90
+DC_MAX_SPEED = 40
 DC_MIN_SPEED = 25
-DC_SPEED_INC = 2.5
+DC_SPEED_INC = 1
 
 SERVO_MIN_ANGLE = 45
 SERVO_MAX_ANGLE = 135
 SERVO_ANGLE_INC = 1
 
 def init(car):
-    car.addDC("BackMotor", 23, 24, 25, 50)
+    car.addDC("BackMotor", 23, 24, 25, 30)
     car.addServo("FrontServo", 17, 90)
     # Add motors here as desired
     car.printMotors()
