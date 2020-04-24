6a5ccd29fdebf63e00ce9570fd40b0f08731fc4c
Nick Nelson <nnelson1601@tamu.edu>
2020-04-23 23:59:56 -0500

    try 2

--- config/motor_config.py
+++ config/motor_config.py
@@ -12,7 +12,7 @@ SERVO_ANGLE_INC = 5
 
 def init(car):
     # car.addDC("BackMotor", 24, 23, 25, 50)
-    car.addServo("FrontServo", 17, 2.5)
+    car.addServo("FrontServo", 17, 90)
     # Add motors here as desired
     car.printMotors()
     return
