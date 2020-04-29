a0221c7d7c05df329fce81ffb5bceae6945f2d24
Nick Nelson <nnelson1601@tamu.edu>
2020-04-29 17:07:39 -0500

    Flip Fire Motors

--- config/motor_config.py
+++ config/motor_config.py
@@ -18,7 +18,7 @@ def init(car):
 
     car.addDC("TurretMotor", GPIOin1=19, GPIOin2=13, GPIOen=26, speed=15, maxS=15)
 
-    car.addDC("FireMotor", GPIOin1=3, GPIOin2=4,
+    car.addDC("FireMotor", GPIOin1=4, GPIOin2=3,
               GPIOen=2, speed=100, minS=15)
 
     # name, GPIO, init angle, min angle, max angle, increment
