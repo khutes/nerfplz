4fe3232c9d5bc2cc7a706a8b6109330ffb04b5ee
Nick Nelson <nnelson1601@tamu.edu>
2020-04-29 22:16:42 -0500

    change front servo pin

--- config/motor_config.py
+++ config/motor_config.py
@@ -22,7 +22,7 @@ def init(car):
               GPIOen=18, speed=100, minS=15)
 
     # name, GPIO, init angle, min angle, max angle, increment
-    car.addServo("FrontServo", GPIOin=27, minAngle=55, maxAngle=125, increment=5)
+    car.addServo("FrontServo", GPIOin=26, minAngle=55, maxAngle=125, increment=5)
 
     car.addServo("TiltServo", GPIOin=17, minAngle=75, increment=5)
 
