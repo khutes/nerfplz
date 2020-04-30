29ddb89822b52668d931a676ff70615572da44c8
Nick Nelson <nnelson1601@tamu.edu>
2020-04-29 22:55:57 -0500

    shut off motors

--- config/motor_config.py
+++ config/motor_config.py
@@ -13,18 +13,18 @@ SERVO_ANGLE_INC = 1
 
 def init(car):
     # name, in1, in2, ena, init speed, min speed, max speed
-    car.addDC("BackMotor", GPIOin1=25, GPIOin2=12, GPIOen=8,
-              speed=60, minS=15, maxS=75)  # Flipped directions
+    # car.addDC("BackMotor", GPIOin1=25, GPIOin2=12, GPIOen=8,
+    #           speed=60, minS=15, maxS=75)  # Flipped directions
 
     car.addDC("TurretMotor", GPIOin1=17, GPIOin2=27, GPIOen=22, speed=25, maxS=25)
 
-    car.addDC("FireMotor", GPIOin1=23, GPIOin2=24,
-              GPIOen=18, speed=100, minS=15)
+    # car.addDC("FireMotor", GPIOin1=23, GPIOin2=24,
+    #           GPIOen=18, speed=100, minS=15)
 
     # name, GPIO, init angle, min angle, max angle, increment
-    car.addServo("FrontServo", GPIOin=4, minAngle=55, maxAngle=125, increment=5)
+    car.addServo("FrontServo", GPIOin=26, minAngle=55, maxAngle=125, increment=5)
 
-    car.addServo("TiltServo", GPIOin=5, minAngle=75, increment=5)
+    # car.addServo("TiltServo", GPIOin=5, minAngle=75, increment=5)
 
     car.printMotors()
     return
